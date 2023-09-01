f11e37d0c0c45f5574b7c3c67eb7214c794e68be
Jorge Betancur <jorge.betancur@teads.tv>
2023-09-01 13:58:58 -0500

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  site:"http://127.0.0.1:5500/dist",
+  base:"/"
+});
